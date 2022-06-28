7b1d55d65a8282020a04cb504f14e39b4e5b8b60
Abimbola Olaide <olaideabimbola2021@gmail.com>
2022-06-28 13:11:57 -0700

    0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,12 @@
 /**
  *  * main - Entry point
  *   *
- *    * Description: This code outputs "Programming is like building a multilingual puzzle"
+ *    * Description: prints "Programming is like building a multilingual puzzle"
  *     *
  *      * Return: Always 0 (Success)
  *       */
 int main(void)
 {
-	    puts("\"Programming is like building a multilingual puzzle\n");
-	        return (0);
-}
+		puts("\"Programming is like building a multilingual puzzle\n");
+			return (0);
+} 
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  *  * main - print the size of various types
  *   *
- *    * Description: prints the size of various types on the computer it is compiled
+ *    * Description: This code prints the size of various types on the computer it is compiled
  *     * and run on
  *      *
  *       * Return: Allways 0 (Success)
@@ -11,17 +11,11 @@
 
 int main(void)
 {
-	printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
-	        printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
-		    printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
-		        printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
-			    printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
+		printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
+			printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
+				printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
+					printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
+						printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
 
-			        printf("Size of a char: %lu byte(s)\n",  sizeof(char));
-				    printf("Size of an int: %lu byte(s)\n",  sizeof(int));
-				        printf("Size of a long int: %lu byte(s)\n",  sizeof(long int));
-					    printf("Size of a long long int: %lu byte(s)\n",  sizeof(long long int));
-					        printf("Size of a float: %lu byte(s)\n",  sizeof(float));
-
-						    return (0);
+							return (0);
 }
